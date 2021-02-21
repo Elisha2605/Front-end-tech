876d8ff75bab030dcd30eca1982d171bcf9737a1
Elisha <alvinjohnson@Elisha-Mac.local>
2021-02-21 20:18:28 +0100

    hello

---
+++ src/main/java/com/example/demo/Service/ServiceResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ServiceResponse<T> {
+
+    private String Status;
+    private T data;
+}
